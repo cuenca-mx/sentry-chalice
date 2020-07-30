58e97165d22b208f6e26168be7c16db2a477ccae
Glen <lccbrokha@gmail.com>
2020-07-30 12:25:27 -0500

    best-config

--- setup.py
+++ setup.py
@@ -6,15 +6,6 @@ version = SourceFileLoader(
     'version', 'sentry_chalice/version.py'
 ).load_module()
 
-test_requires = [
-    'pytest==5.4.3',
-    'pytest-cov==2.10.0',
-    'black==19.10b0',
-    'isort[pipfile]==4.3.21',
-    'flake8==3.8.3',
-    'mypy==0.782',
-    'pytest-chalice==0.0.5',
-]
 
 with open('README.md', 'r') as f:
     long_description = f.read()
@@ -34,13 +25,10 @@ setup(
     package_data=dict(cuenca_validations=['py.typed']),
     python_requires='>=3.6',
     install_requires=[
-        'chalice==1.16.0',
-        'sentry-sdk==0.16.2',
+        'chalice>=1.16,<1.17',
+        'sentry-sdk>=0.16,<0.17',
         'dataclasses>=0.6;python_version<"3.7"',
     ],
-    setup_requires=['pytest-runner'],
-    tests_require=test_requires,
-    extras_require=dict(test=test_requires),
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
