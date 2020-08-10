724af5cb181ed3824c8f779e25249aa568878d07
Glen <lccbrokha@gmail.com>
2020-08-10 18:21:53 -0500

    add-client-test

--- tests/test_sentry_chalice.py
+++ tests/test_sentry_chalice.py
@@ -1,25 +1,27 @@
-from pytest_chalice.handlers import RequestHandler
+from chalice.test import Client
 
 
-def test_exception_boom(app, client: RequestHandler) -> None:
-    response = client.get('/boom')
-    assert response.status_code == 500
-    assert response.json == dict(
-        [
-            ('Code', 'InternalServerError'),
-            ('Message', 'An internal server error occurred.'),
-        ]
-    )
+def test_exception_boom(app) -> None:
+    with Client(app) as client:
+        response = client.http.get('/boom')
+        assert response.status_code == 500
+        assert response.json_body == dict(
+            [
+                ('Code', 'InternalServerError'),
+                ('Message', 'An internal server error occurred.'),
+            ]
+        )
 
 
-def test_has_request(app, capture_events, client: RequestHandler):
+def test_has_request(app, capture_events):
     events = capture_events()
+    with Client(app) as client:
 
-    response = client.get('/context')
-    assert response.status_code == 500
+        response = client.http.get('/context')
+        assert response.status_code == 500
 
-    (event,) = events
-    assert event["transaction"] == "api_handler"
-    assert "data" not in event["request"]
-    assert event["request"]["url"] == "awslambda:///api_handler"
-    assert event["request"]["headers"] == {}
+        (event,) = events
+        assert event["transaction"] == "api_handler"
+        assert "data" not in event["request"]
+        assert event["request"]["url"] == "awslambda:///api_handler"
+        assert event["request"]["headers"] == {}
