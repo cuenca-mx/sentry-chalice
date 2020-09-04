c0d2efbd6c34388bab386eec492484acf81a1386
Matin Tamizi <matin@users.noreply.github.com>
2020-09-04 13:35:04 -0500

    wrap the view_function (#22)
    * wrap the view_function
    
    This _should_ work. We attempt to wrap the view function directly—as opposed to copy / pasting the code from Chalice._get_view_function_response
    
    * wrapping-view-function
    
    * up-version
    
    * more-clear-code
    
    * bump chalice version
    
    * bump chalice version
    
    Co-authored-by: Glen <lccbrokha@gmail.com>

--- sentry_chalice/sentry_chalice.py
+++ sentry_chalice/sentry_chalice.py
@@ -1,8 +1,8 @@
 import sys
-import traceback
+from functools import wraps
 
 import chalice
-from chalice import Chalice, ChaliceViewError, Response
+from chalice import Chalice, ChaliceViewError
 from chalice.app import EventSourceHandler as ChaliceEventSourceHandler
 from sentry_sdk._compat import reraise
 from sentry_sdk.hub import Hub
@@ -39,24 +39,11 @@ class EventSourceHandler(ChaliceEventSourceHandler):
 
 
 def _get_view_function_response(app, view_function, function_args):
-    hub = Hub.current
-    client = hub.client
-
-    with hub.push_scope() as scope:
-        try:
-            response = view_function(**function_args)
-            if not isinstance(response, Response):
-                response = Response(body=response)
-            app._validate_response(response)
-        except ChaliceViewError as e:
-            # Any chalice view error should propagate.  These
-            # get mapped to various HTTP status codes in API Gateway.
-            response = Response(
-                body={'Code': e.__class__.__name__, 'Message': str(e)},
-                status_code=e.STATUS_CODE,
-            )
-            hub.flush()
-        except Exception:
+    @wraps(view_function)
+    def wrapped_view_function(**function_args):
+        hub = Hub.current
+        client = hub.client
+        with hub.push_scope() as scope:
             with capture_internal_exceptions():
                 configured_time = (
                     app.lambda_context.get_remaining_time_in_millis()
@@ -64,37 +51,27 @@ def _get_view_function_response(app, view_function, function_args):
                 scope.transaction = app.lambda_context.function_name
                 scope.add_event_processor(
                     _make_request_event_processor(
-                        app.current_request,
+                        app.current_request.to_dict(),
                         app.lambda_context,
                         configured_time,
                     )
                 )
-            exc_info = sys.exc_info()
-            event, hint = event_from_exception(
-                exc_info,
-                client_options=client.options,
-                mechanism={"type": "chalice", "handled": False},
-            )
-            hub.capture_event(event, hint=hint)
-            hub.flush()
-            headers = {}
-            app.log.error(
-                "Caught exception for %s", view_function, exc_info=True
-            )
-            if app.debug:
-                # If the user has turned on debug mode,
-                # we'll let the original exception propagate so
-                # they get more information about what went wrong.
-                stack_trace = ''.join(traceback.format_exc())
-                body = stack_trace
-                headers['Content-Type'] = 'text/plain'
-            else:
-                body = {
-                    'Code': 'InternalServerError',
-                    'Message': 'An internal server error occurred.',
-                }
-            response = Response(body=body, headers=headers, status_code=500)
-    return response
+            try:
+                return view_function(**function_args)
+            except Exception as exc:
+                if isinstance(exc, ChaliceViewError):
+                    raise
+                exc_info = sys.exc_info()
+                event, hint = event_from_exception(
+                    exc_info,
+                    client_options=client.options,
+                    mechanism={"type": "chalice", "handled": False},
+                )
+                hub.capture_event(event, hint=hint)
+                hub.flush()
+                raise
+
+    return wrapped_view_function
 
 
 class ChaliceIntegration(Integration):
@@ -102,7 +79,17 @@ class ChaliceIntegration(Integration):
 
     @staticmethod
     def setup_once():
-        # for @app.route()
-        Chalice._get_view_function_response = _get_view_function_response
+        old_get_view_function_response = Chalice._get_view_function_response
+
+        def sentry_event_response(app, view_function, function_args):
+            wrapped_view_function = _get_view_function_response(
+                app, view_function, function_args
+            )
+
+            return old_get_view_function_response(
+                app, wrapped_view_function, function_args
+            )
+
+        Chalice._get_view_function_response = sentry_event_response
         # for everything else (like events)
         chalice.app.EventSourceHandler = EventSourceHandler
